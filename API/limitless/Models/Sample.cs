c5a4fd7a64c1fed6e7833d5adadc280989f2ff9f
ameser <ameser@gmail.com>
2018-03-22 22:15:31 -0400

    el nambo no hizo nada

---
+++ API/limitless/Models/Sample.cs
@@ -0,0 +1,16 @@
+﻿using limitless.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace limitless.Models
+{
+    public class Sample : ISample
+    {
+
+        public string user { get; set; }
+        public string name { get; set; }
+    }
+}
